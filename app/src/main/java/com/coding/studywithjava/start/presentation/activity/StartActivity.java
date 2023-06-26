4526c2299eb0a1d9be66f48b08f4fdff949f94c7
Sanghyun Yi <njsharry32@gmail.com>
2023-06-26 19:10:06 +0900

    setting: 기본 Start 도메인 패키지 추기 및 엑티비티 구현

---
+++ app/src/main/java/com/coding/studywithjava/start/presentation/activity/StartActivity.java
@@ -0,0 +1,16 @@
+package com.coding.studywithjava.start.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studywithjava.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
